99556336ec0d7ed7b22a6c88fa550b6563361ba1
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-06-08 01:50:01 +0600

    GET, PUT, DELETE quick setup done. closes #3

--- src/main/java/com/dsi/codingtest/loginapp/ui/controllers/UserController.java
+++ src/main/java/com/dsi/codingtest/loginapp/ui/controllers/UserController.java
@@ -2,7 +2,10 @@ package com.dsi.codingtest.loginapp.ui.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +42,19 @@ public class UserController {
         UserRest returnValue = modelMapper.map(createdUser, UserRest.class);
         return returnValue;
     }
+	
+	@GetMapping
+	public String getUser() {
+		return "GET method called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "PUT method called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "DELETE method called";
+	}
 }
